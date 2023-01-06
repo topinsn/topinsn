1b6a1c7778aadb4482bbf3c94068e14e688c8916
topinsn <olaoluwa54@gmail.com>
2023-01-06 16:02:08 -0500

    modified index and body pages

---
+++ body.php
@@ -0,0 +1,76 @@
+<div class="row">
+            <div class="col-md-5 bg-#A39A91">
+                <img src="images/glasses.png" alt="temitope" class="img-fluid">
+            </div>
+            <div class="col-md-7">
+                <h2 style="text-align: center;">RESEARCHER. INVENTOR. AUTHOR.</h2>
+                <p style="text-align: justify;">
+                    Temitope Olaoluwa is a full-stack software developer who started as a linguist passionate about information systems. His passion influenced his choice of dissertation towards the new media for communicative intent and exploration.
+                    <?php 
+                        include_once ("skills.php");
+                    ?>
+
+                </p>
+            </div>
+        </div>
+
+        
+        <!-- Banner (my intent) -->
+        <!-- <div class="row bg-#f2f2f1ff">
+            <div class="col-md-12 text-center">
+
+                <img src="images/banner_1.png" alt="what_I_do" class="img-fluid mt-2 mb-2" width="1350px" height="300px">
+            </div>
+        </div> -->
+        <!-- pix and publication -->
+        <div class="row">
+            <div class="col-md-5">
+                <img src="images/home.png" alt="Temitope_Olaoluwa" class="img-fluid">
+            </div>
+            <div class="col-md-7">
+                <p style="text-align: justify;">
+                    After reading through your job description, I have deduced that you are looking to hire a competent and vastly experienced content writer to promote your business. I can assure you that I can deliver a job that will produce excellent results. <br><br>                    I am a bilingual English Language speaker with a Master degree in English. In attestation to my versatility, Cambridge University has verified my usage of the English Language and rated it native/bilingual as it appears in my profile.
+                    I am a motivated and diligent freelancer, and I pride myself on creating top-notch content that will match your objectives and help you achieve your aim. <br><br> Previously, I have worked on various writing tasks, carrying projects
+                    from concept to completion. I can take on challenging and complex briefs, transforming them into a simple yet effective writing solution that will leave you satisfied and impressed. <br><br> You/’ll get your work done and delivered
+                    in real-time, and I will be available to attend to all your questions, requests, and complaints before, during, and after the project.
+
+                </p>
+            </div>
+        </div>
+
+        <!-- things I like and image -->
+        <div class="row">
+            <div class="col-md-7">
+                <p style="text-align: justify;">
+                    Temitope Olaoluwa is an author of two dissertations bordering on code-mixing and terrorism with joint publishing of a journal article on drug misuse <br><br>Passionate about web and space technologies. Temitope is currently undergoing
+                    intense training to hone his skills in web development. He has a certification in Amazon Web Service Cloud Computing Professional and preparing to take the AWS Solutions Architect Exam this Fall.
+                    <br><br> In his earlier career, Temitope worked as a Project Officer on the Ponseti International Association Grant for clubfoot eradication in Nigeria spanning about 20 states across the country. He also has managed a complex hospital
+                    system, acquiring management facilities across the different organisations for his hospital. He managed over 40 staff members and involve in voluntary community development. He completed his Master’s Degree at the prestigious Obafemi
+                    Awolowo University, Ile-Ife, Nigeria in 2018.
+
+                </p>
+            </div>
+            <div class="col-md-5">
+                <img src="images/church_1 (1).png" alt="Temitope_Olaoluwa" class="img-fluid">
+            </div>
+        </div>
+
+        <!-- pix and other things -->
+        <div class="row">
+            <div class="col-md-5">
+                <img src="images/ponseti.jpg" alt="Temitope_Olaoluwa" class="img-fluid">
+            </div>
+            <div class="col-md-7">
+                <p style="font-family: ubuntu,'Open Sans', sans-serif; text-align: justify;">
+                    Temitope Olaoluwa is an author of two dissertations bordering on code-mixing and terrorism with joint publishing of a journal article on drug misuse <br><br>Passionate about web and space technologies. Temitope is currently undergoing
+                    intense training to hone his skills in web development. He has a certification in Amazon Web Service Cloud Computing Professional and preparing to take the AWS Solutions Architect Exam this Fall.
+                    <br><br> In his earlier career, Temitope worked as a Project Officer on the Ponseti International Association Grant for clubfoot eradication in Nigeria spanning about 20 states across the country. He also has managed a complex hospital
+                    system, acquiring management facilities across the different organisations for his hospital. He managed over 40 staff members and involve in voluntary community development. He completed his Master’s Degree at the prestigious Obafemi
+                    Awolowo University, Ile-Ife, Nigeria in 2018.
+
+                    
+
+                </p>
+                
+            </div>
+        </div>
