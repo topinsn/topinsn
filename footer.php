b14486e22fa955e7df470c732bbd9e0de4f974db
topinsn <olaoluwa54@gmail.com>
2023-01-07 07:26:29 -0500

    modofied footer

--- footer.php
+++ footer.php
@@ -1,36 +1,42 @@
+ <style type="text/css">
+     .footer ul a{
+        color: black;
+        text-decoration: none;
+     }
+ </style>
  <!-- footer -->
         <div class="d-flex justify-content-center">
             <hr width="450px">
         </div>
-        <div class="row text-md-center footer ">
+        <div class="row footer ">
             <div class="col-md-12 d-flex justify-content-center">
                     <h1>connect with me</h1>
             </div>
             <div class="col-md-12 d-flex justify-content-center">
                 <ul id="sm">
-                    <i class="fa-brands fa-github"></i>
-                    <li><a href="https://github.com/topinsn">Github</a></li>
+                    <a href="https://github.com/topinsn"><i class="fa-brands fa-github"></i>
+                    <li class="pe-2">Github</li></a>
                         
-                    <i class="fa-brands fa-twitter"></i>
-                    <li><a href="https://twitter.com/topinsn">Twiter</a></li>
+                    <a href="https://twitter.com/topinsn"><i class="fa-brands fa-twitter"></i>
+                    <li class="pe-2">Twiter</li></a>
 
-                    <i class="fa-brands fa-linkedin"></i>
-                    <li><a href="https://www.linkedin.com/in/topeoyelami/">LinkedIn</a></li>
+                    <a href="https://www.linkedin.com/in/topeoyelami/"><i class="fa-brands fa-linkedin"></i>
+                    <li class="pe-2">LinkedIn</li></a>
 
-                    <i class="fa-solid fa-envelope"></i>
-                    <li><a href="mailto: noreply@temidev.com.ng">Email</a></li>
+                    <a href="mailto: noreply@temidev.com.ng"><i class="fa-solid fa-envelope"></i>
+                    <li>Email</li></a>
                 </ul>
-            </div>       
-                <div class="d-flex justify-content-center mb-2">
-                    <span > (c) Temitope Olaoluwa
+            </div>
+            <div class="d-flex justify-content-center mb-2">
+                <span> &copy; Temitope Olaoluwa
                         <?php
                             $initial_year = 2021;
                             $current_year = date("Y");
                             echo $initial_year. " - " . $current_year;
                             
                         ?> 
-                    </span>
-                </div>
+                </span>
+            </div>
                     
             
         </div>
